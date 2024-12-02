6b94fb26ca045efff1642e6d6b79b8bf38c1e2fa
Dhanush <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-12-03 03:20:18 +0530

    Update: Create new turbo repo as previous was trashed

---
+++ metaverse/apps/http/src/controllers/general.controller.ts
@@ -0,0 +1,92 @@
+import { Request, Response } from "express"
+import { SigninSchema, SignupSchema } from "../types/types"
+import jwt from "jsonwebtoken"
+import client from "@repo/db/client"
+
+export const signUp = async (req: Request, res: Response) => {
+    const parsedData = SignupSchema.safeParse(req.body);
+
+    if(!parsedData.success){
+        res.status(400).json({
+            message: "Input Validation Failed",
+            error: parsedData.error,
+            data: parsedData.data
+        })
+        return
+    }
+
+    try {
+        const user = await client.user.create({
+            data: {
+                username: parsedData.data.username,
+                password: parsedData.data.password,
+                role: parsedData.data.role === 'admin' ? "Admin" : "User"
+            }
+        })
+
+        res.status(200).json({
+            userId: user.id
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: "User Already Exists"
+        })
+    }
+}
+
+export const signIn = async (req: Request, res: Response) => {
+
+    const parsedData = SigninSchema.safeParse(req.body);
+
+    if(!parsedData.success){
+        res.status(400).json({
+            message: "Input Validation Failer"
+        })
+        return
+    }
+
+    try {
+        const user = await client.user.findUnique({
+            where: {
+                username: parsedData.data.username
+            }
+        })
+
+        if(!user){
+            res.status(400).json({
+                message: "User Not Found"
+            })
+            return;
+        }
+
+        const isValid = (): Boolean => {
+            if(user.password == parsedData.data.password){
+                return true;
+            }
+
+            return false
+        }
+
+        if(!isValid){
+            res.status(403).json({
+                message: "Unauthorized User"
+            })
+            return;
+        }
+
+        const token = jwt.sign({
+            id: user.id,
+            username: parsedData.data.username
+        }, process.env.JWT_SECRET as string)
+
+        res.status(200).json({
+            message: "Sign Up Successfull",
+            token
+        })
+    } catch (error) {
+        
+        res.status(500).json({
+            message: "Server is Down"
+        })
+    }
+}
---
+++ metaverse/apps/http/src/routes/general.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { signIn, signUp } from "../controllers/general.controller";
+
+const generalRoutes = Router();
+
+generalRoutes.post('/signup', signUp);  //, signUp
+generalRoutes.post('/signin', signIn);
+generalRoutes.get('/elements', (req, res) => {
+    res.send("Hello TurboRepo");
+});
+generalRoutes.get('/avatars');
+
+export { generalRoutes }
